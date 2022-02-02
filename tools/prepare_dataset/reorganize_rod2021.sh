d3f82a642d6db5b8bf9584389743c6c06c239935
Yizhou Wang <wyzh555@gmail.com>
2022-02-02 01:21:38 -0800

    Create reorganize_rod2021.sh

---
+++ tools/prepare_dataset/reorganize_rod2021.sh
@@ -0,0 +1,19 @@
+# download all zip files and unzip
+unzip TRAIN_RAD_H.zip
+unzip TRAIN_CAM_0.zip
+unzip TEST_RAD_H.zip
+unzip TRAIN_RAD_H_ANNO.zip
+
+# make folders for data and annotations
+mkdir sequences
+mkdir annotations
+
+# rename unzipped folders
+mv TRAIN_RAD_H sequences/train
+mv TRAIN_CAM_0 train
+mv TEST_RAD_H sequences/test
+mv TRAIN_RAD_H_ANNO annotations/train
+
+# merge folders and remove redundant
+rsync -av train sequences/train
+rm -r train
