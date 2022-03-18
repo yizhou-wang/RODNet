82817cc12416eb659a84e131c63a6d6fa3d4cb07
Yizhou Wang <wyzh555@gmail.com>
2022-03-18 00:04:27 -0700

    scripts for processing cruw2022_3ddet data

---
+++ tools/prepare_dataset/cruw-3ddet/organize_dataset.py
@@ -0,0 +1,126 @@
+import os
+import json
+import numpy as np
+import shutil
+
+from cruw import CRUW
+
+
+def load_json(json_path):
+    with open(json_path) as f:
+        json_dict = json.load(f)
+    return json_dict
+
+
+def get_frame_start_end(frame_id_center, win_size, max_frame_id):
+    frame_id_center = int(frame_id_center)
+    frame_id_start = frame_id_center - int(win_size / 2)
+    frame_id_end = frame_id_center + int(win_size / 2)
+
+    if frame_id_start < 0:
+        frame_id_start = 0
+        frame_id_end = 0 + win_size
+    if frame_id_end > max_frame_id:
+        frame_id_end = max_frame_id
+        frame_id_start = frame_id_end - win_size
+
+    return frame_id_start, frame_id_end
+
+
+def process_camera_data(raw_root, out_root, split_name, seq_name, frame_id_start, frame_id_end):
+    for fid, frame_id in enumerate(range(frame_id_start, frame_id_end)):
+        img_path = os.path.join(raw_root, seq_name, 'camera', 'left', '%06d.png' % frame_id)
+        if os.path.exists(img_path):
+            os.makedirs(os.path.join(out_root, split_name, 'camera', 'left'), exist_ok=True)
+            img_path_new = os.path.join(out_root, split_name, 'camera', 'left', '%s_%04d.png' % (seq_name, frame_id))
+            shutil.copyfile(img_path, img_path_new)
+        else:
+            print('Error: %s, image file not found!' % img_path)
+
+
+def process_radar_data(raw_root, out_root, split_name, seq_name, frame_id_start, frame_id_end,
+                       radar_dim, chirp_ids):
+    # process frame and chirp data into one numpy array
+    radar_win = np.zeros(radar_dim, dtype=np.float32)
+    for fid, frame_id in enumerate(range(frame_id_start, frame_id_end)):
+        for cid, chirp_id in enumerate(chirp_ids):
+            npy_path = os.path.join(raw_root, seq_name, 'radar', 'npy', 'ra',
+                                    '%06d_%04d.npy' % (frame_id, chirp_id))
+            if os.path.exists(npy_path):
+                radar_win[fid, cid] = np.load(npy_path)
+            else:
+                print('Error: %s, npy file not found!' % npy_path)
+
+    # save numpy file
+    os.makedirs(os.path.join(out_root, split_name, 'radar'), exist_ok=True)
+    npy_save_path = os.path.join(out_root, split_name, 'radar',
+                                 '%s_%04d-%04d.npy' % (seq_name, frame_id_start, frame_id_end))
+    np.save(npy_save_path, radar_win)
+
+
+def process_label(raw_root, out_root, split_name, seq_name, frame_id_start, frame_id_end):
+    label_convert_win = []
+    for fid, frame_id in enumerate(range(frame_id_start, frame_id_end)):
+        label_path = os.path.join(raw_root, seq_name, 'label', '%06d.json' % frame_id)
+        label_convert_frame = []
+        if os.path.exists(label_path):
+            label_dicts = load_json(label_path)
+            for label_dict in label_dicts:
+                label_convert = {
+                    'obj_type': label_dict['obj_type'],
+                    'loc3d': {
+                        'x': -label_dict['psr']['position']['y'],
+                        'y': -label_dict['psr']['position']['z'],
+                        'z': label_dict['psr']['position']['x']
+                    },
+                    'dim3d': {
+                        'l': label_dict['psr']['scale']['x'],
+                        'w': label_dict['psr']['scale']['y'],
+                        'h': label_dict['psr']['scale']['z']
+                    }
+                }
+                label_convert_frame.append(label_convert)
+
+        else:  # not label file or no object in this frame
+            pass
+
+        label_convert_win.append(label_convert_frame)
+
+    os.makedirs(os.path.join(out_root, split_name, 'label'), exist_ok=True)
+    label_save_path = os.path.join(out_root, split_name, 'label',
+                                   '%s_%04d-%04d.json' % (seq_name, frame_id_start, frame_id_end))
+    with open(label_save_path, 'w') as f:
+        json.dump(label_convert_win, f, indent=2)
+
+
+def process_split(raw_root, out_root, split_name, split_frames, cruw, win_size=16, max_frame_id=1799):
+    chirp_ids = cruw.sensor_cfg.radar_cfg['chirp_ids']
+    n_chirps = len(chirp_ids)
+    ramap_rsize = cruw.sensor_cfg.radar_cfg['ramap_rsize']
+    ramap_asize = cruw.sensor_cfg.radar_cfg['ramap_asize']
+
+    for split_frame in split_frames:
+        print('processing %s ...' % split_frame)
+        seq_name, frame_id_center = split_frame.split('/')
+        frame_id_start, frame_id_end = get_frame_start_end(frame_id_center, win_size, max_frame_id)
+
+        process_camera_data(raw_root, out_root, split_name, seq_name, frame_id_start, frame_id_end)
+
+        radar_dim = (win_size, n_chirps, ramap_rsize, ramap_asize, 2)
+        process_radar_data(raw_root, out_root, split_name, seq_name, frame_id_start, frame_id_end,
+                           radar_dim, chirp_ids)
+
+        process_label(raw_root, out_root, split_name, seq_name, frame_id_start, frame_id_end)
+
+
+if __name__ == '__main__':
+    raw_root = '/mnt/disk2/CRUW_2022'
+    out_root = '/mnt/disk2/CRUW_2022_3DDet'
+    splits = ['train']
+    chirp_ids = [0, 64, 128, 192]
+    json_path = os.path.join(os.getcwd(), '3ddet_cam_rad_03-13-2022.json')
+
+    cruw = CRUW(raw_root, sensor_config_name='sensor_config_rod2021')  # TODO: add new sensor config for CRUW_2022
+    split_dict = load_json(json_path)
+    for split in splits:
+        process_split(raw_root, out_root, split, split_dict[split], cruw)
---
+++ tools/prepare_dataset/cruw-3ddet/visualize_dataset.py
@@ -0,0 +1,110 @@
+import os
+import numpy as np
+import json
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+
+from cruw import CRUW
+from cruw.visualization.draw_rgb import draw_dets
+from cruw.visualization.draw_rf import draw_centers
+from cruw.visualization.utils import generate_colors_rgb
+from cruw.mapping.ops import xz2raidx
+
+
+def load_json(json_path):
+    with open(json_path) as f:
+        json_dict = json.load(f)
+    return json_dict
+
+
+def get_sorted_filenames(folder_path):
+    file_names = os.listdir(folder_path)
+    file_names.sort()
+    return file_names
+
+
+def convert_lidar_to_radar(loc3d_lidar, cruw):
+    x = loc3d_lidar['x']
+    y = loc3d_lidar['y']
+    z = loc3d_lidar['z']
+    xyz_lid = np.array([x, y, z])
+    t_rad2lid = np.array(cruw.sensor_cfg.calib_cfg['t_cl2lid']) - np.array(cruw.sensor_cfg.calib_cfg['t_cl2rad'])
+    xyz_rad = xyz_lid + t_rad2lid
+    return xyz_rad[0], xyz_rad[1], xyz_rad[2]
+
+
+def draw_data_frame(image_path, chirp, anno_dict, save_path, cruw):
+    img = plt.imread(image_path)
+
+    fig = plt.figure()
+    fig.set_size_inches(16, 5)
+    gs = gridspec.GridSpec(1, 2)
+
+    ax1 = plt.subplot(gs[0])
+    ax1.axis('off')
+    draw_dets(ax1, img, [], [])
+    ax1.set_title('RGB Image')
+
+    ax2 = plt.subplot(gs[1])
+    ax2.axis('off')
+    n_obj = len(anno_dict)
+    categories = []
+    center_ids = []
+    for obj_id in range(n_obj):
+        x, y, z = convert_lidar_to_radar(anno_dict[obj_id]['loc3d'], cruw)
+        if z < cruw.range_grid[-1]:  # only show annotations with range grid
+            categories.append(anno_dict[obj_id]['obj_type'])
+            center_ids.append(xz2raidx(x, z, cruw.range_grid, cruw.angle_grid))
+    colors = generate_colors_rgb(n_obj)
+    draw_centers(ax2, chirp, center_ids, colors, texts=categories, normalized=False)
+    ax2.set_title('RF Image')
+
+    fig.subplots_adjust(hspace=0, wspace=0)
+    fig.savefig(save_path)
+    plt.close(fig)
+
+
+def visualize_split(data_root, split, cruw):
+    camera_dir = os.path.join(data_root, split, 'camera', 'left')
+    radar_dir = os.path.join(data_root, split, 'radar')
+    label_dir = os.path.join(data_root, split, 'label')
+    vis_dir = os.path.join(data_root, split, 'vis')
+    os.makedirs(vis_dir, exist_ok=True)
+
+    camera_names = get_sorted_filenames(camera_dir)
+    radar_names = get_sorted_filenames(radar_dir)
+    label_names = get_sorted_filenames(label_dir)
+
+    n_data = len(radar_names)
+    for data_id in range(n_data):
+        print('visualizing %s ...' % radar_names[data_id][:-4])
+        year, date, seq, frames = radar_names[data_id][:-4].split('_')
+        seq_name = '_'.join([year, date, seq])
+        frame_id_start, frame_id_end = frames.split('-')
+        frame_id_start, frame_id_end = int(frame_id_start), int(frame_id_end)
+        radar_path = os.path.join(radar_dir, radar_names[data_id])
+        radar_data_win = np.load(radar_path)
+        win_size, n_chirps, ramap_rsize, ramap_asize, n_channels = radar_data_win.shape
+
+        label_path = os.path.join(label_dir, label_names[data_id])
+        label_dict_win = load_json(label_path)
+
+        for win_id in range(win_size):
+            camera_name = '%s_%04d.png' % (seq_name, frame_id_start + win_id)
+            assert camera_name in camera_names
+            camera_path = os.path.join(camera_dir, camera_name)
+
+            save_path = os.path.join(vis_dir, camera_name)
+            draw_data_frame(camera_path, radar_data_win[win_id, 0], label_dict_win[win_id], save_path, cruw)
+
+
+if __name__ == '__main__':
+    data_root = '/mnt/disk2/CRUW_2022_3DDet'
+    splits = ['train']
+
+    # cruw = CRUW(data_root, sensor_config_name='sensor_config_rod2021')  # TODO: add new sensor config for CRUW_2022
+    config_path = '../../../configs/dataset_configs/sensor_config_cruw2022_3ddet.json'
+    cruw = CRUW(data_root, sensor_config_name=config_path)
+
+    for split in splits:
+        visualize_split(data_root, split, cruw)
